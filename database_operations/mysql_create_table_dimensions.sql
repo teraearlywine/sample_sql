f0ee58b5c2689c880960f798b3634e3566efb5f1
Tera Earlywine <dev@teraearlywine.com>
2024-06-17 17:25:50 -0700

    *

---
+++ database_operations/mysql_create_table_dimensions.sql
@@ -0,0 +1,31 @@
+-- Create dimension table for budgeted categories (i.e. fixed_expenses, variable_expenses, etc.)
+CREATE TABLE IF NOT EXISTS dim_category (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    budget_category VARCHAR(255) NOT NULL,
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create dimension table for budgeted classifications (i.e. food, clothing, etc.)
+CREATE TABLE IF NOT EXISTS dim_classification (
+    classification_id INT PRIMARY KEY AUTO_INCREMENT,
+    classification VARCHAR(255) NOT NULL,
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create dimension table for payment statuses and bill tracking use(i.e. paid, unpaid, etc.)
+CREATE TABLE IF NOT EXISTS dim_payment_status (
+    payment_status_id INT PRIMARY KEY AUTO_INCREMENT,
+    is_paid VARCHAR(50) NOT NULL,
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Create dimension table for type of expense (i.e. rent, utilities, etc.)
+CREATE TABLE IF NOT EXISTS dim_type (
+    type_id INT PRIMARY KEY AUTO_INCREMENT,
+    type VARCHAR(50) NOT NULL,
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
