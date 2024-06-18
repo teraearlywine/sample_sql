94f0b25ff91f26d56b3beedbff6a7f77235c221d
Tera Earlywine <dev@teraearlywine.com>
2024-06-17 18:24:14 -0700

    *

---
+++ database_operations/3_mysql_create_table_fact_expenditures.sql
@@ -0,0 +1,13 @@
+-- Create database fact table that contains budget data 
+CREATE TABLE IF NOT EXISTS dev.fact_expenditures (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    type_id INT,
+    category_id INT,
+    amount DECIMAL(10, 2),
+    budget_type VARCHAR(255),
+    budget_classifications VARCHAR(255),
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (type_id) REFERENCES dim_type(type_id),
+    FOREIGN KEY (category_id) REFERENCES dim_category(category_id)
+);
