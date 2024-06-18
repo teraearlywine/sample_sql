91975ea73b189332666f9f829ed82f63f0529a86
Tera Earlywine <dev@teraearlywine.com>
2024-06-17 18:54:45 -0700

    *

--- database_operations/4_mysql_insert_type.sql
+++ database_operations/4_mysql_insert_type.sql
@@ -1,3 +1,3 @@
 -- Add one time categorical data 
 INSERT INTO dev.dim_type (type) VALUES ("fixed_expenses");
-INSERT INTO dev.dim_type (type) VALUES ("variable_expenses")
+INSERT INTO dev.dim_type (type) VALUES ("variable_expenses");
