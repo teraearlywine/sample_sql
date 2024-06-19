9c04caef0b2e9b7fdfbd2f98f88f55166c3d768b
Tera Earlywine <dev@teraearlywine.com>
2024-06-18 21:59:03 -0700

    *

--- database_operations/2_mysql_create_table_dim_category.sql
+++ database_operations/2_mysql_create_table_dim_category.sql
@@ -1,8 +1,10 @@
 -- Create dimension table for budgeted categories (i.e. fixed_expenses, variable_expenses, etc.)
 CREATE TABLE IF NOT EXISTS dev.dim_category (
     category_id INT PRIMARY KEY AUTO_INCREMENT,
+    type_id INT,
     budget_category VARCHAR(255) NOT NULL,
     created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    update_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (type_id) REFERENCES dim_type(type_id)
 );
 
--- database_operations/4_mysql_insert_budget_categories.sql
+++ database_operations/4_mysql_insert_budget_categories.sql
@@ -1,11 +1,11 @@
--- Add one time categorical data 
-INSERT INTO dev.dim_category (budget_category) VALUES ('Budget Category');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Rent');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Best Egg Loan');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Car Loan');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Student Loan');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Gas & Electricity');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Car Insurance');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Water');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Phone Bill');
-INSERT INTO dev.dim_category (budget_category) VALUES ('Internet');
+-- Add one time categorical data and it's type_id
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Budget Category', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Rent', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Best Egg Loan', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Car Loan', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Student Loan', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Gas & Electricity', 2);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Car Insurance', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Water', 2);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Phone Bill', 1);
+INSERT INTO dev.dim_category (budget_category, type_id) VALUES ('Internet', 1);
